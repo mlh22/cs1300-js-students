9cae35a312dafedd3e88f226590d40bc571fd5ac
Maliha Islam <malihaislam@Malihas-MBP.lan>
2020-10-27 19:58:30 -0400

    a whole mess of confusion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=Q8UIpgl5m6Dl0CeKUXotzUnk3c1-s5G2mKgDAyNdTvI";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,39 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      
+
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+const displayContent = () => {
+  corsPromise().then(
+    (request) =>
+      (request.onload = request.onerror = function () {
+        
+        // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+        console.log(request.response);
+        const plantData = JSON.parse(request.response).data;
+        document.getElementById("info").innerHTML = plantData;
+        var cooldiv = document.createElement('div');
+        cooldiv.setAttribute("id", "test")
+        var p = document.createElement('p');
+        p.innerText = "my gosh this is weird";
+        console.log(p);
+
+      })
+  );
+}
+
+function infoToggle(){
+  displayContent();
+  var x = document.getElementById("info");
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  }
+  else{
+    x.style.display = "none";
+  }
+}
